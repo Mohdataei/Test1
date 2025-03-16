62cd4d3de41c2b68f77f83c4bd7a3d9726dba00a
mohd <mohd.ataei@gmai.com>
2025-03-16 12:46:01 -0500

    class1

---
+++ src/helloWord/Xyz.java
@@ -0,0 +1,11 @@
+package helloWord;
+
+public class Xyz {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hello");
+		System.out.println("hello1");
+	}
+}
---
+++ src/helloWord/Xyz1.java
@@ -0,0 +1,11 @@
+package helloWord;
+
+public class Xyz1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		
+		
+	}
+
+}
